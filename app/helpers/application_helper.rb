617e37d0de39d6f927d223a1a92c54414ad4bdee
Tim Hall <timothypatrickhall@gmail.com>
2016-09-29 03:02:19 +0000

    Artifacts are working, little rough on the eyes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,8 @@ module ApplicationHelper
   def tenant_name(tentant_id)
      Tenant.find(tentant_id).name
   end
+  
+  def s3(tenant_id, artifact_key)
+    link_to artifact_key, "#{artifact_key}", class: 'main-link', target: 'new'
+  end
 end
