e1cf527b92e7791d3636a90489981ee1f5d72586
Tim Hall <timothypatrickhall@gmail.com>
2016-10-08 22:44:37 +0000

    Edit and update Tenant

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
     :sessions => "milia/sessions", 
     :passwords => "milia/passwords", 
   }
+  
+  match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan 
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan 
 
 
   # The priority is based upon order of creation: first created -> highest priority.
