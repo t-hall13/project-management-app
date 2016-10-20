671ae1ed909a7b2e5f4330dec13c9b1d4c8a2a9a
Tim Hall <timothypatrickhall@gmail.com>
2016-10-20 04:31:17 +0000

    Fixed dependent destroy bug in project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ActiveRecord::Base
   belongs_to :tenant
   validates_uniqueness_of :title
   has_many :artifacts, dependent: :destroy
-  has_many :user_projects
-  has_many :users, through: :user_projects
+  has_many :user_projects, dependent: :destroy
+  has_many :users, through: :user_projects, dependent: :destroy
   validate :free_plan_can_only_have_one_project
   
   def free_plan_can_only_have_one_project
